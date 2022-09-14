dce9ef65aff0646383c14154801310ba34a6c808
Chirchir Bitok <douglasbitok@gmail.com>
2022-09-14 20:18:25 +0300

    Backend intergration started

---
+++ Backend/models.py
@@ -0,0 +1,44 @@
+from flask import Flask
+from marshmallow import Schema, fields, pre_load, validate
+from flask_marshmallow import Marshmallow
+from flask_sqlalchemy import SQLAlchemy
+
+
+ma = Marshmallow()
+db = SQLAlchemy()
+
+class User(db.Model):
+    __tablename__ = 'User'
+    __table_args__ = tuple(db.UniqueConstraint('id','username', name='my_ilajil'))
+
+    id = db.Column(db.Integer, primary_key=True)
+    api_key = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(), primary_key=True)
+    first_name = db.Column(db.String())
+    last_name = db.Column(db.String())
+    password = db.Column(db.String())
+    emailaddress = db.Column(db.String())
+    
+    def __init__(self, id, api_key, username, first_name, last_name, password, emailaddress):
+        self.id = id
+        self.api_key = api_key
+        self.username = username
+        self.first_name = first_name
+        self.last_name = last_name
+        self.password = password
+        self.emailaddress = emailaddress
+
+
+    def __init__(self):
+        return "<id {}>", format(self.id)
+
+    def serialize(self):
+        return {
+            'id' :self.id,
+            'api_key' : self.api_key,
+            'username' : self.username,
+            'first_name' : self.last_name,
+            'last_name' : self.last_name,
+            'password' : self.password,
+            'emailadress' : self.emailaddress
+        }
